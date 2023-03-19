b7888a093c2606ebad77522a03c9df95d43d6cdd
Arun Kannan Thilagar <athilaga@sundevils.asu.edu>
2023-03-19 13:46:32 -0700

    removing unnessary lines

--- test.py
+++ test.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 from datetime import timedelta
@@ -13,15 +7,7 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold
 from joblib import dump, load
 
-
-# In[5]:
-
-
-# data=pd.read_csv('C:\\Users\\koushikbaktha\\CSE572\\Project2\\test.csv',header=None)
 data=pd.read_csv('test.csv',header=None)
-
-# In[7]:
-
 
 def createnomealfeaturematrix(non_meal_data):
     index_to_remove_non_meal=non_meal_data.isna().sum(axis=1).replace(0,np.nan).dropna().where(lambda x:x>5).dropna().index
@@ -30,8 +16,7 @@ def createnomealfeaturematrix(non_meal_data):
     index_to_drop_again=non_meal_data_cleaned.isna().sum(axis=1).replace(0,np.nan).dropna().index
     non_meal_data_cleaned=non_meal_data_cleaned.drop(non_meal_data_cleaned.index[index_to_drop_again]).reset_index().drop(columns='index')
     non_meal_feature_matrix=pd.DataFrame()
-    #non_meal_data_cleaned['tau_time']=(24-non_meal_data_cleaned.iloc[:,0:19].idxmax(axis=1))*5
-    #non_meal_data_cleaned['difference_in_glucose_normalized']=(non_meal_data_cleaned.iloc[:,0:19].max(axis=1)-non_meal_data_cleaned.iloc[:,24])/(non_meal_data_cleaned.iloc[:,24])
+    
     power_first_max=[]
     index_first_max=[]
     power_second_max=[]
@@ -46,13 +31,9 @@ def createnomealfeaturematrix(non_meal_data):
         power_third_max.append(sorted_array[-4])
         index_first_max.append(array.index(sorted_array[-2]))
         index_second_max.append(array.index(sorted_array[-3]))
-    #non_meal_feature_matrix['tau_time']=non_meal_data_cleaned['tau_time']
-    #non_meal_feature_matrix['difference_in_glucose_normalized']=non_meal_data_cleaned['difference_in_glucose_normalized']
-    #non_meal_feature_matrix['power_first_max']=power_first_max
+    
     non_meal_feature_matrix['power_second_max']=power_second_max
     non_meal_feature_matrix['power_third_max']=power_third_max
-    #non_meal_feature_matrix['index_first_max']=index_first_max
-    #non_meal_feature_matrix['index_second_max']=index_second_max
     first_differential_data=[]
     second_differential_data=[]
     standard_deviation=[]
@@ -60,29 +41,16 @@ def createnomealfeaturematrix(non_meal_data):
         first_differential_data.append(np.diff(non_meal_data_cleaned.iloc[:,0:24].iloc[i].tolist()).max())
         second_differential_data.append(np.diff(np.diff(non_meal_data_cleaned.iloc[:,0:24].iloc[i].tolist())).max())
         standard_deviation.append(np.std(non_meal_data_cleaned.iloc[i]))
-    #non_meal_feature_matrix['1stDifferential']=first_differential_data
     non_meal_feature_matrix['2ndDifferential']=second_differential_data
     non_meal_feature_matrix['standard_deviation']=standard_deviation
     return non_meal_feature_matrix
 
-
-# In[8]:
-
-
 dataset=createnomealfeaturematrix(data)
 
-
-# In[10]:
-
-
 from joblib import dump, load
-with open('RandomForestClassifier.pickle', 'rb') as pre_trained:
+with open('DecisionTreeClassifier.pickle', 'rb') as pre_trained:
     pickle_file = load(pre_trained)
     predict = pickle_file.predict(dataset)    
     pre_trained.close()
-
-
-# In[13]:
-
 
 pd.DataFrame(predict).to_csv('Result.csv',index=False,header=False)
--- train.py
+++ train.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pandas as pd
 import numpy as np
 from datetime import timedelta
@@ -13,51 +7,19 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import KFold, RepeatedKFold
 from joblib import dump, load
 
-
-# In[2]:
-
-
-#insulin_data_df=pd.read_csv('C:\\Users\\koushikbaktha\\Desktop\\ASU\\CSE572\\Project 2\\Project 2 Files\\InsulinData.csv',low_memory=False,usecols=['Date','Time','BWZ Carb Input (grams)'])
 insulin_data_df=pd.read_csv('InsulinData.csv',low_memory=False,usecols=['Date','Time','BWZ Carb Input (grams)'])
 
-
-# In[3]:
-
-
-#cgm_data_df=pd.read_csv('C:\\Users\\koushikbaktha\\Desktop\\ASU\\CSE572\\Project 2\\Project 2 Files\\CGMData.csv',low_memory=False,usecols=['Date','Time','Sensor Glucose (mg/dL)'])
 cgm_data_df=pd.read_csv('CGMData.csv',low_memory=False,usecols=['Date','Time','Sensor Glucose (mg/dL)'])
-
-
-# In[4]:
-
 
 insulin_data_df['date_time_stamp']=pd.to_datetime(insulin_data_df['Date'] + ' ' + insulin_data_df['Time'])
 cgm_data_df['date_time_stamp']=pd.to_datetime(cgm_data_df['Date'] + ' ' + cgm_data_df['Time'])
 
-
-# In[5]:
-
-
-#insulin_data_df_1=pd.read_csv('C:\\Users\\koushikbaktha\\Desktop\\ASU\\CSE572\\Project 2\\Insulin_patient2.csv',low_memory=False,usecols=['Date','Time','BWZ Carb Input (grams)'])
 insulin_data_df_1=pd.read_csv('Insulin_patient2.csv',low_memory=False,usecols=['Date','Time','BWZ Carb Input (grams)'])
 
-
-# In[6]:
-
-
-#cgm_data_df_1=pd.read_csv('C:\\Users\\koushikbaktha\\Desktop\\ASU\\CSE572\\Project 2\\CGM_patient2.csv',low_memory=False,usecols=['Date','Time','Sensor Glucose (mg/dL)'])
 cgm_data_df_1=pd.read_csv('CGM_patient2.csv',low_memory=False,usecols=['Date','Time','Sensor Glucose (mg/dL)'])
-
-
-# In[7]:
-
 
 insulin_data_df_1['date_time_stamp']=pd.to_datetime(insulin_data_df_1['Date'] + ' ' + insulin_data_df_1['Time'])
 cgm_data_df_1['date_time_stamp']=pd.to_datetime(cgm_data_df_1['Date'] + ' ' + cgm_data_df_1['Time'])
-
-
-# In[57]:
-
 
 def createmealdata(insulin_data_df,cgm_data_df,dateidentifier):
     insulin_df=insulin_data_df.copy()
@@ -80,33 +42,24 @@ def createmealdata(insulin_data_df,cgm_data_df,dateidentifier):
     if dateidentifier==1:
         for idx,i in enumerate(valid_timestamp_list):
             start=pd.to_datetime(i - timedelta(minutes=30))
-            end=pd.to_datetime(i + timedelta(minutes=90))
-            get_date=i.date().strftime('%-m/%-d/%Y')
-            list1.append(cgm_data_df.loc[cgm_data_df['Date']==get_date].set_index('date_time_stamp').between_time(start_time=start.strftime('%-H:%-M:%-S'),end_time=end.strftime('%-H:%-M:%-S'))['Sensor Glucose (mg/dL)'].values.tolist())
+            end=pd.to_datetime(i + timedelta(minutes=120))
+            get_date=i.date().strftime("%#m/%#d/%Y")
+            list1.append(cgm_data_df.loc[cgm_data_df['Date']==get_date].set_index('date_time_stamp').between_time(start_time=start.strftime('%#H:%#M:%#S'),end_time=end.strftime('%#H:%#M:%#S'))['Sensor Glucose (mg/dL)'].values.tolist())
         return pd.DataFrame(list1)
     else:
         for idx,i in enumerate(valid_timestamp_list):
             start=pd.to_datetime(i - timedelta(minutes=30))
-            end=pd.to_datetime(i + timedelta(minutes=90))
+            end=pd.to_datetime(i + timedelta(minutes=120))
             get_date=i.date().strftime('%Y-%m-%d')
             list1.append(cgm_data_df.loc[cgm_data_df['Date']==get_date].set_index('date_time_stamp').between_time(start_time=start.strftime('%H:%M:%S'),end_time=end.strftime('%H:%M:%S'))['Sensor Glucose (mg/dL)'].values.tolist())
         return pd.DataFrame(list1)
-        
-
-
-# In[58]:
-
 
 meal_data=createmealdata(insulin_data_df,cgm_data_df,1)
 meal_data1=createmealdata(insulin_data_df_1,cgm_data_df_1,2)
 meal_data=meal_data.iloc[:,0:24]
 meal_data1=meal_data1.iloc[:,0:24]
 
-
 # ### No-Meal data extraction
-
-# In[60]:
-
 
 def createnomealdata(insulin_data_df,cgm_data_df):
     insulin_no_meal_df=insulin_data_df.copy()
@@ -136,16 +89,8 @@ def createnomealdata(insulin_data_df,cgm_data_df):
             break
     return pd.DataFrame(dataset)
 
-
-# In[61]:
-
-
 no_meal_data=createnomealdata(insulin_data_df,cgm_data_df)
 no_meal_data1=createnomealdata(insulin_data_df_1,cgm_data_df_1)
-
-
-# In[62]:
-
 
 def createmealfeaturematrix(meal_data):
     index=meal_data.isna().sum(axis=1).replace(0,np.nan).dropna().where(lambda x:x>6).dropna().index
@@ -162,8 +107,8 @@ def createmealfeaturematrix(meal_data):
     index_second_max=[]
     power_third_max=[]
     for i in range(len(meal_data_cleaned)):
-        array=abs(rfft(meal_data_cleaned.iloc[:,0:30].iloc[i].values.tolist())).tolist()
-        sorted_array=abs(rfft(meal_data_cleaned.iloc[:,0:30].iloc[i].values.tolist())).tolist()
+        array=abs(rfft(meal_data_cleaned.iloc[:,0:25].iloc[i].values.tolist())).tolist()
+        sorted_array=abs(rfft(meal_data_cleaned.iloc[:,0:25].iloc[i].values.tolist())).tolist()
         sorted_array.sort()
         power_first_max.append(sorted_array[-2])
         power_second_max.append(sorted_array[-3])
@@ -171,13 +116,8 @@ def createmealfeaturematrix(meal_data):
         index_first_max.append(array.index(sorted_array[-2]))
         index_second_max.append(array.index(sorted_array[-3]))
     meal_feature_matrix=pd.DataFrame()
-    # meal_feature_matrix['tau_time']=meal_data_cleaned['tau_time']
-    # meal_feature_matrix['difference_in_glucose_normalized']=meal_data_cleaned['difference_in_glucose_normalized']
-    # meal_feature_matrix['power_first_max']=power_first_max
     meal_feature_matrix['power_second_max']=power_second_max
     meal_feature_matrix['power_third_max']=power_third_max
-    # meal_feature_matrix['index_first_max']=index_first_max
-    # meal_feature_matrix['index_second_max']=index_second_max
     tm=meal_data_cleaned.iloc[:,22:25].idxmin(axis=1)
     maximum=meal_data_cleaned.iloc[:,5:19].idxmax(axis=1)
     list1=[]
@@ -187,22 +127,14 @@ def createmealfeaturematrix(meal_data):
         list1.append(np.diff(meal_data_cleaned.iloc[:,maximum[i]:tm[i]].iloc[i].tolist()).max())
         second_differential_data.append(np.diff(np.diff(meal_data_cleaned.iloc[:,maximum[i]:tm[i]].iloc[i].tolist())).max())
         standard_deviation.append(np.std(meal_data_cleaned.iloc[i]))
-    # meal_feature_matrix['1stDifferential']=list1
     meal_feature_matrix['2ndDifferential']=second_differential_data
     meal_feature_matrix['standard_deviation']=standard_deviation
     return meal_feature_matrix
 
-
-# In[63]:
-
-
 meal_feature_matrix=createmealfeaturematrix(meal_data)
+
 meal_feature_matrix1=createmealfeaturematrix(meal_data1)
 meal_feature_matrix=pd.concat([meal_feature_matrix,meal_feature_matrix1]).reset_index().drop(columns='index')
-
-
-# In[65]:
-
 
 def createnomealfeaturematrix(non_meal_data):
     index_to_remove_non_meal=non_meal_data.isna().sum(axis=1).replace(0,np.nan).dropna().where(lambda x:x>5).dropna().index
@@ -227,13 +159,8 @@ def createnomealfeaturematrix(non_meal_data):
         power_third_max.append(sorted_array[-4])
         index_first_max.append(array.index(sorted_array[-2]))
         index_second_max.append(array.index(sorted_array[-3]))
-    # non_meal_feature_matrix['tau_time']=non_meal_data_cleaned['tau_time']
-    # non_meal_feature_matrix['difference_in_glucose_normalized']=non_meal_data_cleaned['difference_in_glucose_normalized']
-    # non_meal_feature_matrix['power_first_max']=power_first_max
     non_meal_feature_matrix['power_second_max']=power_second_max
     non_meal_feature_matrix['power_third_max']=power_third_max
-    # non_meal_feature_matrix['index_first_max']=index_first_max
-    # non_meal_feature_matrix['index_second_max']=index_second_max
     first_differential_data=[]
     second_differential_data=[]
     standard_deviation=[]
@@ -241,22 +168,13 @@ def createnomealfeaturematrix(non_meal_data):
         first_differential_data.append(np.diff(non_meal_data_cleaned.iloc[:,0:24].iloc[i].tolist()).max())
         second_differential_data.append(np.diff(np.diff(non_meal_data_cleaned.iloc[:,0:24].iloc[i].tolist())).max())
         standard_deviation.append(np.std(non_meal_data_cleaned.iloc[i]))
-    # non_meal_feature_matrix['1stDifferential']=first_differential_data
     non_meal_feature_matrix['2ndDifferential']=second_differential_data
     non_meal_feature_matrix['standard_deviation']=standard_deviation
     return non_meal_feature_matrix
 
-
-# In[66]:
-
-
 non_meal_feature_matrix=createnomealfeaturematrix(no_meal_data)
 non_meal_feature_matrix1=createnomealfeaturematrix(no_meal_data1)
 non_meal_feature_matrix=pd.concat([non_meal_feature_matrix,non_meal_feature_matrix1]).reset_index().drop(columns='index')
-
-
-# In[68]:
-
 
 meal_feature_matrix['label']=1
 non_meal_feature_matrix['label']=0
@@ -270,18 +188,9 @@ for train_index, test_index in kfold.split(principaldata):
     X_train,X_test,y_train,y_test = principaldata.loc[train_index],principaldata.loc[test_index],    dataset.label.loc[train_index],dataset.label.loc[test_index]
     model.fit(X_train,y_train)
     scores_rf.append(model.score(X_test,y_test))
-
-
-# In[69]:
-
-
+print(np.mean(scores_rf)*100)
 classifier=DecisionTreeClassifier(criterion='entropy')
 X, y= principaldata, dataset['label']
 classifier.fit(X,y)
-dump(classifier, 'RandomForestClassifier.pickle')
-
-
-# In[ ]:
-
-
+dump(classifier, 'DecisionTreeClassifier.pickle')
 
